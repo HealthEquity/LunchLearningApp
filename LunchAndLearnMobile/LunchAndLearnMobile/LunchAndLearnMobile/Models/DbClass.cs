51942b44ccbbf3be707acbb2eea3f512bad31601
Cade Cote <cote.cade@gmail.com>
2017-01-04 11:48:08 -0700

    Initial Commit for Lunch And Learn Mobile

---
+++ LunchAndLearnMobile/LunchAndLearnMobile/LunchAndLearnMobile/Models/DbClass.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LunchAndLearnMobile.Models
+{
+    public class DbClass
+    {
+    public int ClassId { get; set; }
+    public string ClassName { get; set; }
+    public string ClassDescription { get; set; }
+    }
+}
