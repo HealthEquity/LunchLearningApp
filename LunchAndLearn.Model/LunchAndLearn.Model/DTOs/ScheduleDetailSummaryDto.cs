34a9a7ab227d56031dfe469142a0777a2e2d2b92
Matt Moody <matt.moody1990@gmail.com>
2017-01-12 16:34:22 -0700

    Added two DTOs for returning detailed responses of schedules. Added code in global.asax to camelCase all property names on HTTP responses.

---
+++ LunchAndLearn.Model/LunchAndLearn.Model/DTOs/ScheduleDetailSummaryDto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LunchAndLearn.Model.DTOs
+{
+  public class ScheduleDetailSummaryDto
+  {
+    public ICollection<ScheduleDetailDto> ScheduleDetailCollection { get; set; } = new List<ScheduleDetailDto>();
+  }
+}
--- LunchAndLearnService/LunchAndLearnService/Global.asax.cs
+++ LunchAndLearnService/LunchAndLearnService/Global.asax.cs
@@ -6,6 +6,7 @@ using System.Web.Http;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using Newtonsoft.Json.Serialization;
 
 namespace LunchAndLearnService
 {
@@ -19,6 +20,11 @@ namespace LunchAndLearnService
       FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
       RouteConfig.RegisterRoutes(RouteTable.Routes);
       BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+      //Added to camelCase all responses from the controllers to make client consumption easier
+      HttpConfiguration config = GlobalConfiguration.Configuration;
+      config.Formatters.JsonFormatter.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
+      config.Formatters.JsonFormatter.UseDataContractJsonSerializer = false;
     }
   }
 }
