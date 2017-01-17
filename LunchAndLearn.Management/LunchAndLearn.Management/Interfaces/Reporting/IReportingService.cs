0f47a243c2da6c7089025209e7388f5ca7e99920
Matt Moody <matt.moody1990@gmail.com>
2017-01-17 13:56:13 -0700

    Added reporting controller with one method and added unit tests surrounding that. Added reporting service interface and implementation of one method. Added that service to unity registration

---
+++ LunchAndLearn.Management/LunchAndLearn.Management/Interfaces/Reporting/IReportingService.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using LunchAndLearn.Model.DTOs.ReportingDTOs;
+
+namespace LunchAndLearn.Management.Interfaces.Reporting
+{
+  public interface IReportingService : IDisposable
+  {
+    OverallInstructorRatingDTO GetAverageRatingForInstructor(int instructorId);
+  }
+}
