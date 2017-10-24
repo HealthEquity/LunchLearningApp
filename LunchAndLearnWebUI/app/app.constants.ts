babbc55f64aadca701eb88950fb06e42191d2977
Matt Moody <matt.moody1990@gmail.com>
2017-10-24 12:39:50 -0600

    changed from http to https

--- LunchAndLearnWebUI/app/app.constants.ts
+++ LunchAndLearnWebUI/app/app.constants.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
  
 // @Injectable()
 export class Configuration {
-    public Server: string = "https://localhost:44386/";
+    public Server: string = "http://localhost:8748/";
     public ApiUrl: string = "api/";
     public ServerWithApiUrl = this.Server + this.ApiUrl;
 }
