0fec38b697deda74f8bd41deef38868b6461bbea
Matt Moody <matt.moody1990@gmail.com>
2017-11-14 13:47:09 -0700

    through step 12

--- LunchAndLearnService/LunchAndLearnService/Startup.cs
+++ LunchAndLearnService/LunchAndLearnService/Startup.cs
@@ -3,21 +3,38 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web.Http;
 using Microsoft.Owin;
+using Microsoft.Owin.Security.OAuth;
 using Owin;
 
 [assembly: OwinStartup(typeof(LunchAndLearnService.Startup))]
 
 namespace LunchAndLearnService
 {
-  public partial class Startup
+  public class Startup
   {
     public void Configuration(IAppBuilder app)
     {
       HttpConfiguration config = new HttpConfiguration();
 
       WebApiConfig.Register(config);
+      app.UseCors(Microsoft.Owin.Cors.CorsOptions.AllowAll);
       app.UseWebApi(config);
     }
 
+    public void ConfigureOAuth(IAppBuilder app)
+    {
+      OAuthAuthorizationServerOptions OAuthServerOptions = new OAuthAuthorizationServerOptions()
+      {
+        AllowInsecureHttp = true,
+        TokenEndpointPath = new PathString("/token"),
+        AccessTokenExpireTimeSpan = TimeSpan.FromDays(1),
+        Provider = new SimpleAuthorizationServerProvider()
+      };
+
+      // Token Generation
+      app.UseOAuthAuthorizationServer(OAuthServerOptions);
+      app.UseOAuthBearerAuthentication(new OAuthBearerAuthenticationOptions());
+
+    }
   }
 }
