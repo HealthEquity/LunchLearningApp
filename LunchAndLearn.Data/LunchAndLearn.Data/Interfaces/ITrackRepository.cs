e90da7a368a70b5c63ced49af9d43fe69a047c18
Cade Cote <cote.cade@gmail.com>
2018-05-02 12:26:45 -0600

    Add getupcoming for tracksession

--- LunchAndLearn.Data/LunchAndLearn.Data/Interfaces/ITrackRepository.cs
+++ LunchAndLearn.Data/LunchAndLearn.Data/Interfaces/ITrackRepository.cs
@@ -1,10 +1,11 @@
-﻿using System.Linq;
+﻿using System.Collections.Generic;
+using System.Linq;
 using LunchAndLearn.Model.DB_Models;
+using LunchAndLearn.Model.DTOs;
 
 namespace LunchAndLearn.Data.Interfaces
 {
   public interface ITrackRepository : IBaseRepository<Track>
   {
-
   }
 }
--- LunchAndLearn.Data/LunchAndLearn.Data/Interfaces/ITrackSessionRepository.cs
+++ LunchAndLearn.Data/LunchAndLearn.Data/Interfaces/ITrackSessionRepository.cs
@@ -1,10 +1,11 @@
-﻿using System.Linq;
+﻿using System.Collections.Generic;
+using System.Linq;
 using LunchAndLearn.Model.DB_Models;
 
 namespace LunchAndLearn.Data.Interfaces
 {
   public interface ITrackSessionRepository : IBaseRepository<TrackSession>
   {
-
+    List<TrackSession> GetUpcoming();
   }
 }
